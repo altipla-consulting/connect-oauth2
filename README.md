a54d9899cdd90b26f4333b4f75519b9e34c27e51
Ernesto Alejo <ernestokarim@gmail.com>
2025-02-28 14:40:12 +0100

    Export internal code.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
+
 # connect-oauth2
+
+[![Go Reference](https://pkg.go.dev/badge/github.com/altipla-consulting/connect-oauth2.svg)](https://pkg.go.dev/github.com/altipla-consulting/connect-oauth2)
+
 Utilities to call Connect services in Go using OAuth2 authentication.
+
+
+## Install
+
+```shell
+go get github.com/altipla-consulting/connect-oauth2
+```
+
+
+## Usage
+
+Authenticate with the default ID token to call Cloud Run services.
+
+```go
+var client = fooconnect.NewFooServiceClient(
+  http.DefaultClient,
+  "https://www.example.com",
+  connect.WithInterceptors(oauth2.GoogleIDToken(env.IsProduction(), "https://www.example.com")),
+)
+```
+
+
+## Contributing
+
+You can make pull requests or create issues in GitHub. Any code you send should be formatted using `make gofmt`.
+
+
+## License
+
+[MIT License](LICENSE)
---
+++ google.go
@@ -0,0 +1,39 @@
+package oauth2
+
+import (
+	"context"
+	"fmt"
+
+	"connectrpc.com/connect"
+	"golang.org/x/oauth2"
+	"google.golang.org/api/idtoken"
+)
+
+// GoogleIDToken adds an ID token as a bearer header to the requests. It needs to check for production environments to
+// avoid trying to generate an id token in the local computer where it's not available.
+func GoogleIDToken(isProduction bool, scope string) connect.Interceptor {
+	var ts oauth2.TokenSource
+	var initErr error
+	if !isProduction {
+		ts, initErr = idtoken.NewTokenSource(context.Background(), scope)
+	}
+
+	return connect.UnaryInterceptorFunc(func(next connect.UnaryFunc) connect.UnaryFunc {
+		return connect.UnaryFunc(func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
+			if initErr != nil {
+				return nil, fmt.Errorf("connect-oauth2: cannot initialize token source: %w", initErr)
+			}
+
+			if !isProduction {
+				return next(ctx, req)
+			}
+
+			token, err := ts.Token()
+			if err != nil {
+				return nil, fmt.Errorf("connect-oauth2: cannot retrieve token: %w", err)
+			}
+			req.Header().Set("Authorization", "Bearer "+token.AccessToken)
+			return next(ctx, req)
+		})
+	})
+}
