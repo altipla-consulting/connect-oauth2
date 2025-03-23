33192c2cd7990e21604879db4898af0b990f0aa6
Ernesto Alejo <ernestokarim@gmail.com>
2025-03-23 18:48:27 +0100

    Reload local authentication every 50 minutes.

--- google.go
+++ google.go
@@ -5,11 +5,28 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+	"time"
 
 	"connectrpc.com/connect"
 	"golang.org/x/oauth2"
 	"google.golang.org/api/idtoken"
 )
+
+var _ oauth2.TokenSource = (*localTokenSource)(nil)
+
+type localTokenSource struct{}
+
+func (l *localTokenSource) Token() (*oauth2.Token, error) {
+	cmd := exec.Command("gcloud", "auth", "print-identity-token")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, fmt.Errorf("connect-oauth2: cannot retrieve local user token: %w", err)
+	}
+	return &oauth2.Token{
+		AccessToken: strings.TrimSpace(string(output)),
+		Expiry:      time.Now().Add(time.Minute * 50),
+	}, nil
+}
 
 // GoogleIDToken adds an ID token as a bearer header to the requests. It needs to check for production environments to
 // avoid trying to generate an id token in the local computer where it's not available.
@@ -19,13 +36,7 @@ func GoogleIDToken(isProduction bool, scope string) connect.Interceptor {
 	if isProduction {
 		ts, initErr = idtoken.NewTokenSource(context.Background(), scope)
 	} else {
-		cmd := exec.Command("gcloud", "auth", "print-identity-token")
-		output, err := cmd.Output()
-		if err != nil {
-			initErr = fmt.Errorf("connect-oauth2: cannot retrieve local user token: %w", err)
-		} else {
-			ts = oauth2.StaticTokenSource(&oauth2.Token{AccessToken: strings.TrimSpace(string(output))})
-		}
+		ts = oauth2.ReuseTokenSource(nil, new(localTokenSource))
 	}
 
 	return connect.UnaryInterceptorFunc(func(next connect.UnaryFunc) connect.UnaryFunc {
